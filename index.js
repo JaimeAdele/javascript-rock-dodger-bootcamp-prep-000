745d2e23a5688b0f66462f10fab2c3cfd1150ea4
Jaime Zuspann <jaimeadele@gmail.com>
2017-12-08 04:45:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function createRock(x) {
    * seems like a good pace.)
    */
   function moveRock() {
-    rock.style.top = `${top + 2}px`;
+    //rock.style.top = `${top + 2}px`;
     // implement me!
     // (use the comments below to guide you!)
     /**
@@ -84,7 +84,7 @@ function createRock(x) {
      */
      if (checkCollision(rock)){
        endGame();
-     };
+     } //else if;
 
 
     /**
