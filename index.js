c674d72fc44bda53c86ac75cf66e297847a8a297
Jaime Zuspann <jaimeadele@gmail.com>
2017-12-08 05:28:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function createRock(x) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
-     if (top == 400){
+     if (top == 380){
        rock.remove();
      }
   }
